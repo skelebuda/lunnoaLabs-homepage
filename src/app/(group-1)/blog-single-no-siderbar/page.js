3e70582b1b0888723e531c903a8b710c3fe5be5b
Kelebuda <skelebuda@gmail.com>
2024-12-28 17:30:35 +0100

    test

--- src/app/(group-1)/blog-single-no-siderbar/page.js
+++ src/app/(group-1)/blog-single-no-siderbar/page.js
@@ -3,7 +3,7 @@ import BlogArtical from '@/components/sections/blogs/blogArtical'
 import PageTitle from '@/components/sections/pageTitle'
 
 export const metadata = {
-    title: "NextPro | Blog-Single-2",
+    title: "Lunnoa Labs | Blog",
     description: "NextPro is a modern Next.js and Tailwind CSS Template there features General Marketing, Social Media Marketing, AI in Marketing, Paid Advertising, Video Marketing, Analytics and Reporting, Industry news & Trends, E-commerce Marketing",
   };
   
